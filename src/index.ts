91dafd07c66281f64bab715a956730ecbb307f45
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2023-07-04 21:49:10 +0200

    feat: add reexports for `defaultEndpoint`, `defaultTlsEndpoint`, `defaultScriptUrlPattern`

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { ICache, Cacheable, LocalStorageCache, SessionStorageCache, InMemoryCach
 export * from './client'
 export * from './global'
 
+export { defaultEndpoint, defaultTlsEndpoint, defaultScriptUrlPattern } from '@fingerprintjs/fingerprintjs-pro'
 export type { Agent, LoadOptions, GetOptions, GetResult, ExtendedGetResult } from '@fingerprintjs/fingerprintjs-pro'
