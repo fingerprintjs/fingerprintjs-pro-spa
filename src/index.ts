735e302f0280950122de8ab8983828e35db676a2
Przemysław Żydek <przemyslawzydek@gmail.com>
2023-10-09 17:45:34 +0300

    feat: export `fingerprintjs-pro` as `FingerprintJSPro`, deprecate other exports from this library

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,64 @@ export { ICache, Cacheable, LocalStorageCache, SessionStorageCache, InMemoryCach
 export * from './client'
 export * from './global'
 
-export { defaultEndpoint, defaultTlsEndpoint, defaultScriptUrlPattern } from '@fingerprintjs/fingerprintjs-pro'
-export type { Agent, LoadOptions, GetOptions, GetResult, ExtendedGetResult } from '@fingerprintjs/fingerprintjs-pro'
+import * as FingerprintJSPro from '@fingerprintjs/fingerprintjs-pro'
+
+/**
+ * @deprecated
+ *
+ * Use `FingerprintJSPro.defaultEndpoint` instead, this export will be removed in the next major version
+ * */
+const defaultEndpoint = FingerprintJSPro.defaultEndpoint
+
+/**
+ * @deprecated
+ *
+ * Use `FingerprintJSPro.defaultTlsEndpoint` instead, this export will be removed in the next major version
+ */
+const defaultTlsEndpoint = FingerprintJSPro.defaultTlsEndpoint
+
+/**
+ * @deprecated
+ *
+ * Use `FingerprintJSPro.defaultScriptUrlPattern` instead, this export will be removed in the next major version
+ */
+const defaultScriptUrlPattern = FingerprintJSPro.defaultScriptUrlPattern
+
+export { defaultEndpoint, defaultTlsEndpoint, defaultScriptUrlPattern }
+
+export { FingerprintJSPro }
+
+/**
+ * @deprecated
+ *
+ * Use `FingerprintJSPro.Agent` instead, this export will be removed in the next major version
+ * */
+export type Agent = FingerprintJSPro.Agent
+
+/**
+ * @deprecated
+ *
+ * Use `FingerprintJSPro.GetOptions` instead, this export will be removed in the next major version
+ */
+export type GetOptions<TExtended extends boolean, TIP = unknown> = FingerprintJSPro.GetOptions<TExtended, TIP>
+
+/**
+ * @deprecated
+ *
+ * Use `FingerprintJSPro.GetResult` instead, this export will be removed in the next major version
+ */
+export type GetResult = FingerprintJSPro.GetResult
+
+/**
+ * @deprecated
+ *
+ * Use `FingerprintJSPro.LoadOptions` instead, this export will be removed in the next major version
+ */
+export type LoadOptions = FingerprintJSPro.LoadOptions
+
+/**
+ * @deprecated
+ *
+ * Use `FingerprintJSPro.ExtendedGetResult` instead, this export will be removed in the next major version
+ */
+export type ExtendedGetResult = FingerprintJSPro.ExtendedGetResult
